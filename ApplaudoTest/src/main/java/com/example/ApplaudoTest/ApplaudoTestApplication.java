5118402c551d362f02573ed76b66779f600a620a
Federico Alvarez <alvarez.federico93@gmail.com>
2021-12-31 19:25:34 -0600

    add ApplaudoTest project

---
+++ ApplaudoTest/src/main/java/com/example/ApplaudoTest/ApplaudoTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApplaudoTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplaudoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplaudoTestApplication.class, args);
+	}
+
+}
