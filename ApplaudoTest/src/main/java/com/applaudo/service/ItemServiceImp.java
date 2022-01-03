46cf9e6a500ac7b24912acf825c8357e43853e1b
Federico Alvarez <alvarez.federico93@gmail.com>
2022-01-03 12:13:34 -0600

    add logs and comments on services

--- ApplaudoTest/src/main/java/com/applaudo/service/ItemServiceImp.java
+++ ApplaudoTest/src/main/java/com/applaudo/service/ItemServiceImp.java
@@ -14,8 +14,13 @@ import com.applaudo.repository.ItemRepository;
 import com.applaudo.service.definition.ItemService;
 import com.applaudo.utils.ApiResult;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @Service
 public class ItemServiceImp implements ItemService{
+	
+	Logger log = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
 
 	static final String ITEM_EXIST = "Item exists on database";
 	static final String ITEM_NOT_FOUND = "Item not found";
@@ -24,8 +29,19 @@ public class ItemServiceImp implements ItemService{
 	@Autowired
 	private ItemRepository itemRepository;
 	
+	
+	/**
+	 * This method insert data and return the inserted item.
+	 * 
+	 * @param item Object with data to save
+	 * @return ResponseEntity<> Object with the response.
+	 * 
+	 */
+	
 	@Override
 	public ResponseEntity<ApiResult> saveItem(Item item) {
+		
+		log.info("-- saveItem --");
 		
 		item.setItemEnteredDate(LocalDateTime.now());
 		item.setItemLastModifiedDate(LocalDateTime.now());
@@ -38,8 +54,17 @@ public class ItemServiceImp implements ItemService{
 		
 	}
 
+	/**
+	 * This method return a specific item.
+	 * 
+	 * @param itemId Id item to search 
+	 * @return ResponseEntity<> Object with the response.
+	 * 
+	 */
 	@Override
 	public ResponseEntity<ApiResult> getItem(long itemId) {
+		
+		log.info("-- getItem {} --",itemId);
 		
 		Optional<Item> item = itemRepository.findById(itemId);
 		if(item.isPresent())
@@ -48,8 +73,19 @@ public class ItemServiceImp implements ItemService{
 		return new ResponseEntity<>(generateApiResult(String.valueOf(HttpStatus.NOT_FOUND.value()),ITEM_NOT_FOUND,null), HttpStatus.NOT_FOUND);
 	}
 
+	/**
+	 * This method return a List items in base two parameters.
+	 * 
+	 * @param itemStatus Item status to search, can be AVAILABLE or SOLD
+	 * @param itemName Name of item to search, ignore case.
+	 * @return ResponseEntity<> Object with the response.
+	 * 
+	 */
 	@Override
 	public ResponseEntity<ApiResult> getItems(ItemStatus itemStatus, String itemName) {
+		
+		log.info("-- getItems by {} and {} --",itemStatus,itemName);
+		
 		return new ResponseEntity<>(generateApiResult(String.valueOf(HttpStatus.OK.value()),SUCCESS,itemRepository.findByItemStatusAndItemNameIgnoreCase(itemStatus,itemName)),HttpStatus.OK);
 		
 	}
